6671ac3545f0cbf57f4254b368e3657f2adbced3
NONGSANG\nongsang <shdtkd37@gamil.com>
2026-04-21 16:54:02 +0900

    데이터 가져오기 기능 추가

--- 15ToDoList/app/src/main/java/com/example/a15todolist/MainActivity.kt
+++ 15ToDoList/app/src/main/java/com/example/a15todolist/MainActivity.kt
@@ -6,11 +6,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
 import com.example.a15todolist.databinding.ActivityMainBinding
+import com.example.a15todolist.db.AppDatabase
+import com.example.a15todolist.db.ToDoDao
+import com.example.a15todolist.db.ToDoEntity
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var db: AppDatabase
+    private lateinit var toDoDao: ToDoDao
+    private lateinit var toDoList: ArrayList<ToDoEntity>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,5 +38,28 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        db = AppDatabase.getInstance(this)!!
+        toDoDao = db.getToDoDao()
+
+        getAllToDoList()
+    }
+
+    private fun getAllToDoList() {
+        {
+            Thread {
+                toDoList = ArrayList(toDoDao.getAll())
+                setRecyclerView()
+            }.start()
+        }
+
+        lifecycleScope.launch {
+            toDoList = ArrayList(toDoDao.getAll())
+            setRecyclerView()
+        }
+    }
+
+    private fun setRecyclerView() {
+
     }
 }
