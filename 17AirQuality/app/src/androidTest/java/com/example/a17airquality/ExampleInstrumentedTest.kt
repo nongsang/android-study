89064d49ace3e5b0ed9169ac8a4185dac7c6f6c8
NONGSANG\nongsang <shdtkd37@gamil.com>
2026-09-23 03:11:14 +0900

    미세먼지 측정 앱 v2.0.1 테마 버전 업그레이드

--- 17AirQuality/app/src/androidTest/java/com/example/a17airquality/ExampleInstrumentedTest.kt
+++ 17AirQuality/app/src/androidTest/java/com/example/a17airquality/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.a16airquality", appContext.packageName)
+        assertEquals("com.example.a17AirQuality", appContext.packageName)
     }
 }
